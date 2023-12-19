061bd6e1b51d48ae6768cb8af5ee591c6b49a532
GennadyVenidiktov <wenter3@outlook.com>
2023-12-19 22:09:25 +0500

    Отладка контекста безопасности

--- src/main/java/ru/venidiktov/spring/security/detail/Application.java
+++ src/main/java/ru/venidiktov/spring/security/detail/Application.java
@@ -3,6 +3,7 @@ package ru.venidiktov.spring.security.detail;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@EnableWebSecurity(debug = true) // Включает дебаг и логирование в SpringSecurity
 @SpringBootApplication
 public class Application {
 
